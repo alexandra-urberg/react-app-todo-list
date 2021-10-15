31aeae10e0faf630aabbe2d67ac2d3349083021b
Sasha Stepanova <chernishevskie@yandex.ru>
2021-10-16 02:01:27 +0300

    feat: added timer

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react';
+import Timer from './Timer.js';
 
 const Form = ({ inputText, setInputText, tasks, setTasks, setStatus }) => {
     const [validationErrors, setValidationErrors] = useState({inputText: ''});
@@ -9,7 +10,7 @@ const Form = ({ inputText, setInputText, tasks, setTasks, setStatus }) => {
         let errors = validationErrors;
         setInputText(e.target.value); 
 
-        value.length < 2 ? errors.inputText = 'Минимальное колличество символоа - 2': errors.inputText = '' && setValidationErrors(errors);// проверяем на минимальное колличество символов
+        value.length < 2 ? errors.inputText = 'Minimum amount of symbols - 2': errors.inputText = '' && setValidationErrors(errors);// проверяем на минимальное колличество символов
     };
 
     const submitTasksHandler = (e) => { //save new value
@@ -31,35 +32,39 @@ const Form = ({ inputText, setInputText, tasks, setTasks, setStatus }) => {
 
     return(
         <form className="form">
-            <label className="popup__label">
-                <input
-                 type="text"
-                 name="inputText"
-                 placeholder="please, write you task"
-                 required
-                 value={inputText || ""}
-                 onChange={inputTextHandler} 
-                 className="task__input"
-                />
-                <span className={`${validationErrors.inputText ? "popup__input-error" : null}`}>{validationErrors.inputText}</span>
-            </label>
-            <button 
-             onClick={submitTasksHandler} 
-             type="submit"
-             className={`tasks-button ${validationErrors.inputText || inputText === '' ? "button_disabled" : null}`} 
-            >
-                <i className="fas fa-plus-square"></i>
-            </button>
-            <div className="select">
-                <select 
-                 onChange={statusHandler}
-                 className="todos filter-todo"
+            <h2> What do we need to do?</h2>
+            <div className="form__container">
+                <label className="label">
+                    <input
+                     type="text"
+                     name="inputText"
+                     placeholder="please, write you task"
+                     required
+                     value={inputText || ""}
+                     onChange={inputTextHandler} 
+                     className="task__input"
+                    />
+                <button 
+                 onClick={submitTasksHandler} 
+                 type="submit"
+                 className={`tasks-button ${validationErrors.inputText || inputText === '' ? "button_disabled" : null}`} 
                 >
-                    <option value="all">All</option>
-                    <option value="completed">Completed</option>
-                    <option value="uncompleted">Uncompleted</option>
-                </select>
+                    <i className="fas fa-plus-square"></i>
+                </button>
+                </label>
+                <div className="select">
+                    <select 
+                     onChange={statusHandler}
+                     className="todos filter-todo"
+                    >
+                        <option value="all">All</option>
+                        <option value="completed">Completed</option>
+                        <option value="uncompleted">Uncompleted</option>
+                    </select>
+                </div>
             </div>
+            <span className={`${validationErrors.inputText ? "popup__input-error" : null}`}>{validationErrors.inputText}</span>
+            <Timer />
         </form>
     );
 };
